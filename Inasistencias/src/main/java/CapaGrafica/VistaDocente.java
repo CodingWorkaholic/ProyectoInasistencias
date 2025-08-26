cc0d6326041788f6f0851c5208172034b20024ef
Workaholic of Codes <abogorodskiy@hotmail.com>
2025-08-26 14:28:00 -0300

    yes

--- Inasistencias/src/main/java/CapaGrafica/VistaDocente.java
+++ Inasistencias/src/main/java/CapaGrafica/VistaDocente.java
@@ -29,6 +29,12 @@ public class VistaDocente extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(22, 33, 88));
@@ -38,6 +44,53 @@ public class VistaDocente extends javax.swing.JFrame {
 
         jPanel1.setBackground(new java.awt.Color(22, 33, 88));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Lucida Sans Unicode", 1, 36)); // NOI18N
+        jLabel1.setText("¿Cómo quieres gestionar tus inasistencias?");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 60, 830, -1));
+
+        jButton1.setBackground(new java.awt.Color(143, 227, 255));
+        jButton1.setFont(new java.awt.Font("Lucida Sans Unicode", 1, 20)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(0, 0, 0));
+        jButton1.setText("Modificar");
+        jButton1.setAlignmentY(0.0F);
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 230, 160, 60));
+
+        jButton2.setBackground(new java.awt.Color(143, 227, 255));
+        jButton2.setFont(new java.awt.Font("Lucida Sans Unicode", 1, 20)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(0, 0, 0));
+        jButton2.setText("Registrar");
+        jButton2.setAlignmentY(0.0F);
+        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 230, 160, 60));
+
+        jButton3.setBackground(new java.awt.Color(143, 227, 255));
+        jButton3.setFont(new java.awt.Font("Lucida Sans Unicode", 1, 20)); // NOI18N
+        jButton3.setForeground(new java.awt.Color(0, 0, 0));
+        jButton3.setText("Eliminar");
+        jButton3.setAlignmentY(0.0F);
+        jPanel1.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 230, 160, 60));
+
+        jButton4.setBackground(new java.awt.Color(143, 227, 255));
+        jButton4.setFont(new java.awt.Font("Lucida Sans Unicode", 1, 20)); // NOI18N
+        jButton4.setForeground(new java.awt.Color(0, 0, 0));
+        jButton4.setText("Volver");
+        jButton4.setAlignmentY(0.0F);
+        jPanel1.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 430, 160, 60));
+
+        jPanel2.setForeground(new java.awt.Color(30, 30, 30));
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 780, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 210, Short.MAX_VALUE)
+        );
+
+        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 160, 780, 210));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -79,6 +132,12 @@ public class VistaDocente extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
 }
