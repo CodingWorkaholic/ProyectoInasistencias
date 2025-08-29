1e80cc9d78312ea397a78a1d9443cc7dd5b1ef08
Workaholic of Codes <abogorodskiy@hotmail.com>
2025-08-29 07:49:53 -0300

    asdf

--- Inasistencias/src/main/java/CapaLogica/Inasistencias.java
+++ Inasistencias/src/main/java/CapaLogica/Inasistencias.java
@@ -1,7 +1,6 @@
 package CapaLogica;
 
 public class Inasistencias {
-<<<<<<< Updated upstream
     private String id, fechaInicio, fechaFin;
 
     public String getId() {
@@ -26,23 +25,5 @@ public class Inasistencias {
 
     public void setFechaFin(String fechaFin) {
         this.fechaFin = fechaFin;
-=======
-    private String ci, pass;
-
-    public String getCi() {
-        return ci;
->>>>>>> Stashed changes
     }
-
-    public void setCi(String ci) {
-        this.ci = ci;
-    }
-
-    public String getPass() {
-        return pass;
-    }
-
-    public void setPass(String pass) {
-        this.pass = pass;
-    }    
 }
